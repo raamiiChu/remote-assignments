dfb29bcdd9cedd2e922191f22df7ccd118621a6e
raamiiChu <ericchu0617@gmail.com>
2023-10-02 18:24:38 +0800

    remote-week2: add some comments

--- remote-week2/app.js
+++ remote-week2/app.js
@@ -4,14 +4,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Home Page
 app.get("/", (req, res) => {
     res.send("Home Page");
 });
 
+// Healthcheck API
 app.get("/healthcheck", (req, res) => {
     res.send("OK");
 });
 
+// Routing for All
 app.get("*", (req, res) => {
     res.send("404 page not found");
 });
