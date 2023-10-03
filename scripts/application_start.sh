a58f7aa81f9f7980eabd9ea3309a7e9f6ccb0ed0
raamiiChu <ericchu0617@gmail.com>
2023-10-03 16:32:52 +0800

    Fix: file path

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+#give permission for everything in the appwork-school directory
+sudo chmod -R 777 /home/ec2-user/appwork-school
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/appwork-school
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
