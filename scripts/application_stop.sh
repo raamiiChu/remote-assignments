17d949e0975b9a478a3db81d2c40ce823f94ecbe
raamiiChu <ericchu0617@gmail.com>
2023-10-03 16:43:23 +0800

    Fix: permission

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-sudo pkill node
+pids=$(pgrep node)
+if [ -n "$pids" ]; then
+    for pid in $pids; do
+        kill -9 "$pid"
+    done
+else
+    echo "No node servers running"
+fi
