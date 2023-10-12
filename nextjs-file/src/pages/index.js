e3bb81dda9b39981fce6695960f65ab5e2eb8868
raamiiChu <ericchu0617@gmail.com>
2023-10-12 22:49:15 +0800

    Update: home page

--- nextjs-file/src/pages/index.js
+++ nextjs-file/src/pages/index.js
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
+import PostsContainer from "./components/getPostData.js";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <div className=""></div>;
+    return (
+        <main className={`${inter.className}`}>
+            <PostsContainer />
+        </main>
+    );
 }
